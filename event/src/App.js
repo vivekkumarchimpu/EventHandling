ad4ac0a7fa42d24434ada967ea1a8d8c23378048
vivekkumarchimpu <vivekdada1502@gmail.com>
2024-11-30 13:48:02 +0530

    input checkbox handling

--- event/src/App.js
+++ event/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ButtonHandling from './components/ButtonHandling';
 import PropsHandling from './components/PropsHandling';
+import InputCheckbox from './components/InputCheckbox';
 
 function App() {
   return (
     <>
       <ButtonHandling />
       <PropsHandling />
+      <InputCheckbox />
     </>
   );
 }
---
+++ event/src/components/InputCheckbox.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+function InputCheckbox() {
+    const [value, setValue] = useState("");
+    const clickHandling = (e) => {
+        setValue(e.target.value);
+    }
+    // useEffect(() => {
+    //     console.log(value);
+    // },[value])
+    return (
+        <>
+            <div className='mainbox'>
+                <ul>
+                    <li><input type='checkbox' name='first' value={"first"} onClick={clickHandling} /><span>Select first</span></li>
+                    <li><input type='checkbox' name='second' value={"second"} onClick={clickHandling} /><span>Select second</span></li>
+                    <li><input type='checkbox' name='third' value={"third"} onClick={clickHandling} /><span>Select third</span></li>
+                    <li><input type='checkbox' name='fourth' value={"fourth"} onClick={clickHandling}/><span>Select fourth</span></li>
+                </ul>
+                <h1>{value}</h1>
+
+            </div>
+
+        </>
+    )
+}
+
+export default InputCheckbox
